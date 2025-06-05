e77b58103714898fa8d9781d32e27703c313365b
KwakDADA <down022@gmail.com>
2025-06-05 11:02:30 +0900

    [Setting] STAMPIT-48 - 도메인 모델 추가

---
+++ StampIt-Project/StampIt-Project/Domain/Model/Group.swift
@@ -0,0 +1,16 @@
+//
+//  Group.swift
+//  StampIt-Project
+//
+//  Created by 곽다은 on 6/5/25.
+//
+
+import Foundation
+
+struct Group {
+    let groupID: String
+    let members: [Member]
+    let leaderID: String
+    let nameChangedAt: Date
+}
+
