c497efeb230126f82ad5f23d78a9628c55cda3fa
KwakDADA <down022@gmail.com>
2025-06-06 10:58:18 +0900

    [Feat] STAMPIT-49 - Mission 도메인 모델 정의

---
+++ StampIt-Project/StampIt-Project/Domain/Model/Mission.swift
@@ -0,0 +1,17 @@
+//
+//  Mission.swift
+//  StampIt-Project
+//
+//  Created by 곽다은 on 6/6/25.
+//
+
+import Foundation
+
+struct Mission {
+    let missionID: String
+    let title: String
+    let asignedTo: String
+    let asignedBy: String
+    let dueDate: Date
+    let status: MissionStatus
+}
