a5dac7bb43537e19d3ac3a98e1d1e0a3f25614aa
YunJuHyung <wngud5289@naver.com>
2025-06-07 21:02:37 +0900

    [Feat] STAMPIT-15- SendInviteViewModel 구현

---
+++ StampIt-Project/StampIt-Project/Presentation/Inviting/ViewModel/SendInviteViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  SendInviteViewModel.swift
+//  StampIt-Project
+//
+//  Created by 윤주형 on 6/7/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import UIKit
+
+
+class SendInviteViewModel: ViewModelProtocol {
+
+    // MARK: - Action
+    enum Action {
+        case copyButtonTapped
+    }
+
+    // MARK: - State
+    struct State {
+        let inviteCode: BehaviorRelay<String>
+        let showCopyMessage: PublishRelay<String>
+    }
+
+    var disposeBag = DisposeBag()
+
+    private let actionSubject = PublishSubject<Action>()
+    var action: AnyObserver<Action> {
+        return actionSubject.asObserver()
+    }
+
+    let state: State
+
+    // MARK: - Init
+    init() {
+        let inviteCode = Self.generateInviteCode()
+        let inviteCodeRelay = BehaviorRelay<String>(value: inviteCode)
+        let showCopyMessageRelay = PublishRelay<String>()
+
+        self.state = State(
+            inviteCode: inviteCodeRelay,
+            showCopyMessage: showCopyMessageRelay
+        )
+
+        // Action 처리
+        actionSubject
+            .subscribe(onNext: { action in
+                switch action {
+                case .copyButtonTapped:
+                    UIPasteboard.general.string = inviteCode
+                    showCopyMessageRelay.accept("초대 코드가 복사되었습니다")
+                }
+            }).disposed(by: disposeBag)
+    }
+
+    // MARK: - Helper
+        private static func generateInviteCode(length: Int = 8) -> String {
+            let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+            return String((0..<length).compactMap { _ in chars.randomElement() })
+        }
+
+
+}
