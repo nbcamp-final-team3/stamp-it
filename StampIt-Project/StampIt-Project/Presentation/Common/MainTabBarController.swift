2a5937e870b549d22e8f586e14a865f6b34046b2
amazingDove367 <hortenssiaa@gmail.com>
2025-06-19 08:30:36 +0900

    [Feat] STAMPIT-106 Tapped 상태의 TabBarItem 이미지 적용

--- StampIt-Project/StampIt-Project/Presentation/Common/MainTabBarController.swift
+++ StampIt-Project/StampIt-Project/Presentation/Common/MainTabBarController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class MainTabBarController: UITabBarController {
     
     // MARK: - Properties
     
     private let container: DIContainer
+    private let disposeBag = DisposeBag()
     
     // MARK: - Initializer, Deinit, requiered
     
@@ -29,16 +32,56 @@ final class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabs()
+        bindTapSelection()
     }
     
     // MARK: - Methods
+    
+    private func bindTapSelection() {
+        rx.didSelect
+            .compactMap { [weak self] viewController in
+                self?.viewControllers?.firstIndex(of: viewController)
+            }.subscribe(with: self) { owner, index in
+                owner.updateTabBarImage(selectedIndex: index)
+            }.disposed(by: disposeBag)
+    }
+    
+    private func updateTabBarImage(selectedIndex: Int) {
+        guard let tabItems = tabBar.items else { return }
+        
+        for (index, item) in tabItems.enumerated() {
+            if index == selectedIndex {
+                item.image = UIImage(named: selectedImageName(for: index))?.withRenderingMode(.alwaysOriginal)
+            } else {
+                item.image = UIImage(named: defaultImageName(for: index))?.withRenderingMode(.alwaysOriginal)
+            }
+        }
+    }
+    
+    private func selectedImageName(for index: Int) -> String {
+        switch index {
+        case 0: return "tabBarHomeTapped"
+        case 1: return "tabBarMissionTapped"
+        case 2: return "tabBarMyPageTapped"
+        default: return ""
+        }
+    }
+    
+    private func defaultImageName(for index: Int) -> String {
+        switch index {
+        case 0: return "tabBarHome"
+        case 1: return "tabBarMission"
+        case 2: return "tabBarMyPage"
+        default: return ""
+        }
+    }
     
     private func setupTabs() {
         let homeVC = container.makeHomeViewController()
         let homeNav = UINavigationController(rootViewController: homeVC)
         homeNav.tabBarItem = UITabBarItem(
             title: "홈",
-            image: UIImage(named: "tabBarHome")!.withTintColor(.red),
+            image: UIImage(named: "tabBarHomeTapped")!.withTintColor(.red),
             tag: 0
         )
         
