9bb054123ac341acca9bcc05d069bb12cedc0088
LEEBUYONG <dqd001@naver.com>
2025-06-20 09:49:03 +0900

    [Style] STAMPIT-112 - TODO, Test 끝난 print문 삭제

--- StampIt-Project/StampIt-Project/App/DIContainer.swift
+++ StampIt-Project/StampIt-Project/App/DIContainer.swift
@@ -9,7 +9,6 @@ import UIKit
 import Foundation
 
 // MARK: - 의존성 주입 컨테이너
-// TODO: DIContainer 합치기 전에 사용하실 분들은 아래에 추가하시면 되고, 나중에 합칠때 전체 수정될 예정이니 참고 바랍니다.
 final class DIContainer {
 
     // MARK: - Managers (Infrastructure Layer)
