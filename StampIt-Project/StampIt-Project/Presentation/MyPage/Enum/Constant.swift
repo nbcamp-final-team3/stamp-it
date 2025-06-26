f95ed2967d1945122a4756b6071a226a682703c2
amazingDove367 <hortenssiaa@gmail.com>
2025-06-26 16:11:40 +0900

    [Refactor] STAMPIT-165 Constant 수정 및 추가

--- StampIt-Project/StampIt-Project/Presentation/MyPage/Enum/Constant.swift
+++ StampIt-Project/StampIt-Project/Presentation/MyPage/Enum/Constant.swift
@@ -5,23 +5,15 @@
 //  Created by kingj on 6/9/25.
 //
 
-import Foundation
+import UIKit
 
-enum TabType: Int {
-    case stampBoard = 0
-    case profile = 1
-    
-    var title: String {
-        switch self {
-        case .stampBoard: return "스탬프판"
-        case .profile: return "프로필"
-        }
-    }
-}
+// MARK: - Sticker
 
 extension StickerType {
     static let imageSize: CGFloat = 53
 }
+
+// MARK: - MyPage
 
 enum MyPage {
     enum Tab {
@@ -65,6 +57,20 @@ enum MyPage {
     }
 }
 
+enum TabType: Int {
+    case stampBoard = 0
+    case profile
+    
+    var title: String {
+        switch self {
+        case .profile: return "프로필"
+        case .stampBoard: return "스탬프판"
+        }
+    }
+}
+
+// MARK: - Navigation
+
 enum Navigation {
     static let fontSize: CGFloat = 24
     static let height: Double = 68
@@ -75,4 +81,35 @@ enum Navigation {
     static let appLogo: String = "AppLogo"
     static let appLogoWidth: CGFloat = 125
     static let appLogoHeight: CGFloat = 42
+}
+
+// MARK: - StampBoard
+
+
+
+enum StampBoard: Int {
+    case red = 0
+    case blue
+    case yellow
+    case purple
+    
+    var bgColor: UIColor {
+        switch self {
+        case .red: return .red50
+        case .blue: return .blue50
+        case .yellow: return .yellow50
+        case .purple: return .purple50
+        }
+    }
+    
+    var pageBar: UIColor {
+        switch self {
+        case .red: return .red200
+        case .blue: return .blue200
+        case .yellow: return .yellow200
+        case .purple: return .purple200
+        }
+    }
+    
+    static var totalPage: Int { 4 }
 }
