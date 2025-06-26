d05859db0d7514a00d9431be5b75683d8a4873a0
YunJuHyung <wngud5289@naver.com>
2025-06-26 16:32:07 +0900

    [Feat] STAMPIT-118 멤버쉽 쿼리 메서드 추가

---
+++ StampIt-Project/StampIt-Project/Domain/Repository/GroupManageRepository.swift
@@ -0,0 +1,8 @@
+//
+//  GroupManageRepository.swift
+//  StampIt-Project
+//
+//  Created by 윤주형 on 6/26/25.
+//
+
+import Foundation
---
+++ StampIt-Project/StampIt-Project/Domain/UseCaseImpl/GroupManageUseCaseImpl.swift
@@ -0,0 +1,8 @@
+//
+//  GroupManageUseCaseImpl.swift
+//  StampIt-Project
+//
+//  Created by 윤주형 on 6/26/25.
+//
+
+import Foundation
