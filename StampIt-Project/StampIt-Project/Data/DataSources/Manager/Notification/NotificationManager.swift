d388d4bc5381d3447be070cebafa647e5fc6db4e
LEEBUYONG <dqd001@naver.com>
2025-06-30 01:20:02 +0900

    [Feat] STAMPIT-126 - Notification 로컬 구현
    호출: NotificationManager.shared.sendLocalNotification

--- StampIt-Project/StampIt-Project/Data/DataSources/Manager/Notification/NotificationManager.swift
+++ StampIt-Project/StampIt-Project/Data/DataSources/Manager/Notification/NotificationManager.swift
@@ -5,19 +5,56 @@
 //  Created by iOS study on 6/27/25.
 //
 
+import Foundation
+import FirebaseMessaging
 import UserNotifications
-import AVFoundation
 
-final class NotificationManager {
+class NotificationManager: ObservableObject {
     static let shared = NotificationManager()
     
-    func requestNotificationPermission() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if granted {
-                print("알림 권한 허용됨")
-            } else {
-                print("알림 권한 거부됨")
+    @Published var fcmToken: String = ""
+    @Published var isNotificationEnabled: Bool = false
+    
+    private init() {
+        getFCMToken()
+        checkNotificationStatus()
+    }
+    
+    func checkNotificationStatus() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            DispatchQueue.main.async {
+                self.isNotificationEnabled = settings.authorizationStatus == .authorized
             }
         }
+    }
+    
+    func getFCMToken() {
+        if let token = UserDefaults.standard.string(forKey: "FCMToken") {
+            self.fcmToken = token
+        }
+        
+        Messaging.messaging().token { token, _ in
+            if let token = token {
+                DispatchQueue.main.async {
+                    self.fcmToken = token
+                }
+            }
+        }
+    }
+    
+    func sendLocalNotification(title: String, body: String, userInfo: [String: Any] = [:]) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = .default
+        content.userInfo = userInfo
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        )
+        
+        UNUserNotificationCenter.current().add(request)
     }
 }
