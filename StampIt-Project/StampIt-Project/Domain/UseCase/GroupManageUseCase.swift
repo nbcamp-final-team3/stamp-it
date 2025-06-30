dcefb0ccb1990a0341b26e21c1341723122242a7
YunJuHyung <wngud5289@naver.com>
2025-07-01 02:46:25 +0900

    [Feat] STAMPIT-177 멤버 관리 메서드 선언부

--- StampIt-Project/StampIt-Project/Domain/UseCase/GroupManageUseCase.swift
+++ StampIt-Project/StampIt-Project/Domain/UseCase/GroupManageUseCase.swift
@@ -23,4 +23,20 @@ protocol GroupManageUseCase {
     
     /// 그룹 간 이동 (현재 그룹 탈퇴 후 새 그룹 가입)
     func switchToNewGroup(inviteCode: String) -> Observable<Void>
+    
+    /// 그룹 멤버 관리 초기 데이터 로드
+    func loadGroupMemberManageData() -> Observable<GroupMemberManageData>
+    
+    /// 멤버 ID로 멤버 내보내기
+    func exportMember(memberId: String) -> Observable<Void>
+    
+    /// 멤버 목록 새로고침
+    func refreshMembers() -> Observable<[Member]>
+}
+
+// MARK: - Data Models
+struct GroupMemberManageData {
+    let currentUser: User
+    let members: [Member]
+    let isLeader: Bool
 }
