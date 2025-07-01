61296efec9b9cd54c8232fe26c6e341c4e89b653
YunJuHyung <wngud5289@naver.com>
2025-07-01 15:36:50 +0900

    [Feat] STAMPIT-180 다인 그룹에서 그룹 이동시 사용자 데이터 정리 문서화
    - InviteUseCaseImpl

--- StampIt-Project/StampIt-Project/Domain/UseCaseImpl/InviteUseCaseImpl.swift
+++ StampIt-Project/StampIt-Project/Domain/UseCaseImpl/InviteUseCaseImpl.swift
@@ -124,6 +124,21 @@ final class InviteUseCaseImpl: InviteUseCase {
                             profileImage: user.profileImage ?? "profileImage1"
                         )
                     }
+                // TODO: 사용자 데이터 정리: 다인 그룹에서 탈퇴하는 경우
+                // 📄 참고: Notion 육남매 대피소 > 유저 그룹 이동 시 시나리오 문서화
+                // 🔧 필요시 주석 해제하여 활성화
+//                    .flatMap { [weak self] _ -> Observable<Void> in
+//                        guard let self = self else { return .empty() }
+//                        if oldGroupMemberCount > 1 {
+//                            return self.inviteRepository.cleanupUserDataWithRetry(
+//                                userId: user.userID,
+//                                groupId: oldGroupId,
+//                                maxRetries: 3
+//                            )
+//                        } else {
+//                            return .just(())
+//                        }
+//                    }
                     .flatMap {
                         self.fetchInvite(inviteCode: inviteCode)
                     }
