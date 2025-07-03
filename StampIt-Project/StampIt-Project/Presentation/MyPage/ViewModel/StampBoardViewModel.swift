28696184a553e7e62d979b84e6131e09ccccdb82
amazingDove367 <hortenssiaa@gmail.com>
2025-07-03 16:18:05 +0900

    [Feat] STAMPIT-182 Zigzag 생성 전, createdAt 내림차순으로 스티커 정렬
    - 스티커 생성 기준: 미션 완료한 순서

--- StampIt-Project/StampIt-Project/Presentation/MyPage/ViewModel/StampBoardViewModel.swift
+++ StampIt-Project/StampIt-Project/Presentation/MyPage/ViewModel/StampBoardViewModel.swift
@@ -24,7 +24,7 @@ final class StampBoardViewModel: ViewModelProtocol {
     
     struct State {
         let user = BehaviorRelay<User?>(value: nil)
-        let stickersByPage = BehaviorRelay<[[Sticker]]>(value: .init())
+        let stickersByPage = BehaviorRelay<[[StickerUI]]>(value: .init())
         let tabType = BehaviorRelay<TabType>(value: .stampBoard)
         let stickerSummary = BehaviorRelay<(collected: Int, completed: Int)>(value: (.zero, .zero))
     }
@@ -70,7 +70,7 @@ final class StampBoardViewModel: ViewModelProtocol {
         
         /// stickerSummary, stickers 가 동시에 변경
         myPageUseCase.observeStickerCount(userId: user.userID)
-            .flatMapLatest { [weak self] count -> Observable<(Int, [[Sticker]])> in
+            .flatMapLatest { [weak self] count -> Observable<(Int, [[StickerUI]])> in
                 guard let self else { return .empty() }
                 
                 let completedBoard = Int(count / StampBoardSection.totalStamp)
@@ -98,23 +98,11 @@ final class StampBoardViewModel: ViewModelProtocol {
                 
                 return Observable.combineLatest(stickerObservables)
                     .map { stickerLists in
-                        var formattedStickers: [[Sticker]] = .init()
+                        var formattedStickers: [[StickerUI]] = .init()
                         for (page, stickers) in stickerLists.enumerated() {
                             formattedStickers.append(
                                 stickers.enumerated().map { (index, sticker) in
-                                    Sticker(
-                                        userID: sticker.userID,
-                                        stickerID: sticker.stickerID,
-                                        groupId: sticker.groupId,
-                                        month: sticker.month,
-                                        type: StickerType.from(page),
-//                                        type: index.isMultiple(of: 2) ? StickerType.stampBlue : StickerType.stampYellow,
-                                        pinNumber: sticker.pinNumber,
-                                        createdAt: sticker.createdAt,
-                                        missionId: sticker.missionId,
-                                        maxStickers: sticker.maxStickers,
-                                        assignedBy: sticker.assignedBy
-                                    )
+                                    StickerUI.map(sticker, type: StickerType.from(page))
                                 }
                             )
                         }
@@ -140,62 +128,20 @@ final class StampBoardViewModel: ViewModelProtocol {
             }.disposed(by: disposeBag)
     }
     
-    private func updateStickerZigzag(_ stickers: [[Sticker]]) {
-        let zigzagged: [[Sticker]] = stickers.map {
-            makeZigzagOrder(
-                from: $0,
-                columns: StampBoardSection.column
-            )
-        }
-        
-        print("zigzag stickers: \(zigzagged)")
-        state.stickersByPage.accept(zigzagged)
-    }
-    
-    private func makeZigzagOrder(from stickers: [Sticker], columns: Int) -> [Sticker] {
-        let totalStickerCount = StampBoardSection.totalStamp
-        let totalStickers: [Sticker] = {
-            (0..<totalStickerCount).map { index in
+    private func updateStickerZigzag(_ stickerLists: [[StickerUI]]) {
+        let zigzagged: [[StickerUI]] = stickerLists
+            .map { stickers in
+                /// createdAt 내림차순 기준 정렬
+                let ordered = stickers
+                    .sorted { $0.createdAt > $1.createdAt }
                 
-                /// Empty stickers 배열 일 때 default stamp 생성
-                if stickers.count == .zero {
-                    return makeEmptySticker()
-                } else {
-                    /// stickers 배열이 1 이상, totalStickerCount 이하 일 경우
-                    if index < stickers.count {
-                        return stickers[index]
-                    } else {
-                        return makeEmptySticker()
-                    }
-                }
-            }
-        }()
-        
-        let rows = stride(from: 0, to: totalStickers.count, by: columns)
-            .map {
-                Array(totalStickers[$0..<min($0 + columns, totalStickers.count)])
+                return StickerUtil.makeZigzagOrder(
+                    from: ordered,
+                    columns: StampBoardSection.column,
+                    pinNumber: state.stickerSummary.value.completed
+                )
             }
         
-        let ordered = rows.enumerated().flatMap { (index, row) in
-            index.isMultiple(of: 2) ? row : row.reversed()
-        }
-        return ordered
-    }
-    
-    private func makeEmptySticker() -> Sticker {
-        
-        // TODO: type 체크
-        Sticker(
-            userID: "유저아이디",
-            stickerID: "\(UUID())",
-            groupId: "그룹아이디",
-            month: "2025-05",
-            type: .stampGray,
-            pinNumber: state.stickerSummary.value.completed,
-            createdAt: Date(),
-            missionId: "미션아이디",
-            maxStickers: 30,
-            assignedBy: "미션준유저"
-        )
+        state.stickersByPage.accept(zigzagged)
     }
 }
