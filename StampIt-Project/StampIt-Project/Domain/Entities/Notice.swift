3e21a2fe478dab611ce9b27ef47cb6634292ac7d
YunJuHyung <wngud5289@naver.com>
2025-07-08 22:12:01 +0900

    [Feat] STAMPIT-219 Notice 모델 세팅

---
+++ StampIt-Project/StampIt-Project/Domain/Entities/Notice.swift
@@ -0,0 +1,37 @@
+//
+//  Notice.swift
+//  StampIt-Project
+//
+//  Created by 윤주형 on 7/8/25.
+//
+
+import Foundation
+
+struct DomainNotice {
+    let id: String
+    let title: String
+    let description: String
+    let category: NoticeCategory
+    let createdAt: Date
+    let isRead: Bool
+}
+
+enum NoticeCategory: String {
+    case newMission      // 새로운 미션 알림
+    case missionRequest   // 미션 조르기 알림
+    case member          // 멤버 관련 알림
+    // ...추가 케이스
+    case unknown
+}
+
+extension DomainNotice {
+    func toDomain() -> DomainNotice{
+        return DomainNotice(
+            id: id,
+            title: title,
+            description: description,
+            category: NoticeCategory(rawValue: category) ?? .unknown,
+            createdAt: timestamp.dateValue()
+        )
+    }
+}
