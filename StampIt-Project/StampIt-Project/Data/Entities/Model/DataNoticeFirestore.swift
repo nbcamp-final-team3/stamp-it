face51194f36d2e7435ece63f5214ca02fa0bb98
YunJuHyung <wngud5289@naver.com>
2025-07-08 22:12:01 +0900

    [Feat] STAMPIT-219 Notice 모델 세팅

---
+++ StampIt-Project/StampIt-Project/Data/Entities/Model/DataNoticeFirestore.swift
@@ -0,0 +1,32 @@
+//
+//  DataNoticeFirestore.swift
+//  StampIt-Project
+//
+//  Created by 윤주형 on 7/8/25.
+//
+
+import Foundation
+import FirebaseCore
+
+struct DataNoticeFirestore {
+    let id: String
+    let title: String
+    let description: String
+    let category: String
+    let createdAt: Timestamp
+    let url: String
+    let isRead: Bool
+}
+
+extension DataNoticeFirestore {
+    func toDomain() -> DomainNotice {
+        return DomainNotice(
+            id: id,
+            title: title,
+            description: description,
+            category: NoticeCategory(rawValue: category) ?? .unknown,
+            createdAt: createdAt.dateValue(),
+            isRead: isRead
+        )
+    }
+}
