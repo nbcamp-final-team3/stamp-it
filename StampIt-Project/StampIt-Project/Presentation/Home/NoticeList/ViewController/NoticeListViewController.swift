6cab369950b4b3215d2f4ffc88a7078fd45fe7d5
다은 <down022@gmail.com>
2025-07-28 09:57:49 +0900

    [Feat] STAMPIT-221 커스텀네비바 추가

--- StampIt-Project/StampIt-Project/Presentation/Home/NoticeList/ViewController/NoticeListViewController.swift
+++ StampIt-Project/StampIt-Project/Presentation/Home/NoticeList/ViewController/NoticeListViewController.swift
@@ -6,30 +6,53 @@
 //
 
 import UIKit
+import SnapKit
 
 final class NoticeListViewController: UIViewController {
 
     // MARK: - Dependencies
 
-    let viewModel: NoticeListViewModel!
+    private let viewModel: NoticeListViewModel!
 
     // MARK: - UI Components
 
-    let noticeView = NoticeView()
+    private let navigationBar = DefaultNavigationBar(.titleWithBackButton(title: "알림"))
+    private let noticeView = NoticeView()
 
     // MARK: - Life Cycles
 
     init(viewModel: NoticeListViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+        setStyle()
+        setHierarchy()
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func loadView() {
-        view = noticeView
+    private func setStyle() {
+        view.backgroundColor = .FFFFFF
     }
 
+    private func setHierarchy() {
+        [
+            navigationBar,
+            noticeView,
+        ].forEach { view.addSubview($0) }
+    }
+
+    private func setConstraints() {
+        navigationBar.snp.makeConstraints { make in
+            make.top.directionalHorizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+
+        noticeView.snp.makeConstraints { make in
+            make.top.equalTo(navigationBar.snp.bottom)
+            make.directionalHorizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalToSuperview()
+        }
+    }
 }
