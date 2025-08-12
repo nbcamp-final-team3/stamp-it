bb581642861e97e4e5c2dc6cc1c1f5365b1a4f39
leebuyong <dqd001@naver.com>
2025-08-12 12:09:41 +0900

    [Fix] STAMPIT-226 - 미적용된 HomeMissionWidget 코드 수정(view)

--- StampIt-Project/MissionWidget/MissionWidgetView.swift
+++ StampIt-Project/MissionWidget/MissionWidgetView.swift
@@ -66,7 +66,7 @@ struct MissionWidgetEntryView: View {
 }
 
 struct MissionRowView: View {
-    let mission: MissionWidgetUI
+    let mission: HomeMissionWidget
     
     var body: some View {
         HStack(alignment: .top, spacing: 12) {
