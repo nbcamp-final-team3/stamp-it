ed85aa9d7c006c62e5179c13d3485c234abb5f23
다은 <down022@gmail.com>
2025-08-17 20:24:00 +0900

    [Feat] STAMPIT-221 화면 이동 시 탭바 숨김 처리

--- StampIt-Project/StampIt-Project/Presentation/Home/NoticeList/ViewController/NoticeListViewController.swift
+++ StampIt-Project/StampIt-Project/Presentation/Home/NoticeList/ViewController/NoticeListViewController.swift
@@ -31,15 +31,31 @@ final class NoticeListViewController: UIViewController {
     init(viewModel: NoticeListViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         setStyle()
         setHierarchy()
         setConstraints()
         bind()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    // MARK: - Set Style
 
     private func setStyle() {
         view.backgroundColor = .FFFFFF
