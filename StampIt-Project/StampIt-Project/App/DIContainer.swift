fff4ed4de7229637cc19e8af3f67edad59f1b3fd
다은 <down022@gmail.com>
2025-08-17 17:50:35 +0900

    [Feat] STAMPIT-221 NoticeNagivator 정의

--- StampIt-Project/StampIt-Project/App/DIContainer.swift
+++ StampIt-Project/StampIt-Project/App/DIContainer.swift
@@ -208,7 +208,7 @@ final class DIContainer {
         return OnboardingViewModel(totalPages: 3)
     }
 
-    func makeMyMissionViewModel(user: User, memberCache: [String: Member]) -> MyMissionViewModel {
+    func makeMyMissionViewModel(memberCache: [String: Member]) -> MyMissionViewModel {
         return MyMissionViewModel(
             memberCache: memberCache,
             useCase: myMissionUseCase,
@@ -291,8 +291,8 @@ final class DIContainer {
         return OnboardingViewController(viewModel: viewModel)
     }
 
-    func makeMyMissionViewController(user: User, memberCache: [String: Member]) -> MyMissionViewController {
-        let viewModel = makeMyMissionViewModel(user: user, memberCache: memberCache)
+    func makeMyMissionViewController(memberCache: [String: Member]) -> MyMissionViewController {
+        let viewModel = makeMyMissionViewModel(memberCache: memberCache)
         return MyMissionViewController(viewModel: viewModel)
     }
 
