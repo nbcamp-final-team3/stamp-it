728f462bf5d2b3c106d873951dad0c5fee21c670
YunJuHyung <wngud5289@naver.com>
2025-08-22 17:03:25 +0900

    [Fix] STAMPIT-238 InviteUseCase 롤백

--- StampIt-Project/StampIt-Project/Domain/UseCase/InviteUseCase.swift
+++ StampIt-Project/StampIt-Project/Domain/UseCase/InviteUseCase.swift
@@ -9,10 +9,8 @@ import Foundation
 import RxSwift
 
 protocol InviteUseCase {
-    /// 현재 사용자 정보 조회
-    func getCurrentUser() -> Observable<User?>
-    /// 초대 코드 확인 메서드 - 사용자 정보와 초대 정보를 함께 반환
-    func acceptInvite(inviteCode: String) -> Observable<(User, Invite)>
+    /// 초대 코드 확인 메서드
+    func acceptInvite(inviteCode: String) -> Observable<Invite>
     /// 초대코드 생성 메서드
     func getInviteCode() -> Observable<String>
     /// 다인 그룹 입장 시 확인이 필요한지 확인하는 메서드
